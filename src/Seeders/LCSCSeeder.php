f1ae1252e7dc57507274124d8876e6145878af91
Fatih Toprak <patron@fatihtoprak.com>
2022-02-24 20:58:10 +0300

    Add dynamic remote source file method

--- src/Seeders/LCSCSeeder.php
+++ src/Seeders/LCSCSeeder.php
@@ -12,7 +12,17 @@ class LCSCSeeder extends Seeder
     public function run()
     {
         ini_set('memory_limit', '-1');
-        $countries = json_decode(file_get_contents(__DIR__ . "/../countries+states+cities.json"));
+
+
+        $remoteJson = 'https://raw.githubusercontent.com/dr5hn/countries-states-cities-database/master/countries+states+cities.json';
+
+        $fileSize = $this->remoteFileSize($remoteJson);
+
+        $this->info('Fetching countries from remote source...['.$fileSize.']');
+
+
+        $countries = $this->remoteGet($remoteJson);
+        //$countries = json_decode(file_get_contents(__DIR__ . "/../countries+states+cities.json"));
         foreach ($countries as $countryData) {
             $country = Country::query()->updateOrCreate(
                 [
@@ -32,7 +42,7 @@ class LCSCSeeder extends Seeder
                 "latitude" => $countryData->latitude,
                 "longitude" => $countryData->longitude,
             ]);
-            echo "Adding " . $country->name . " state and cities... ";
+            $this->info("Adding " . $country->name . " state and cities... ");
 
             foreach ($countryData->states as $stateData) {
                 $state = State::query()->updateOrCreate(
@@ -58,8 +68,78 @@ class LCSCSeeder extends Seeder
                     ]);
                 }
             }
-            echo "done." . PHP_EOL;
+            $this->info("done." . PHP_EOL);
         }
 
+    }
+
+    /**
+     * Get Remote File
+     *
+     * @param [string] $url
+     * @return void
+     */
+    private function remoteGet( $url ) : string
+    {
+        $ch = curl_init();
+
+        curl_setopt($ch, CURLOPT_AUTOREFERER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, TRUE);
+
+        $data = curl_exec($ch);
+        curl_close($ch);
+
+        return $data;
+    }
+
+    /**
+     * Get file size from remote source
+     *
+     * @param [string] $url
+     * @return void
+     */
+    private function remoteFileSize( $url )
+    {
+        $ch = curl_init($url);
+
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_HEADER, TRUE);
+        curl_setopt($ch, CURLOPT_NOBODY, TRUE);
+
+        $data = curl_exec($ch);
+        $size = curl_getinfo($ch, CURLINFO_CONTENT_LENGTH_DOWNLOAD);
+
+        curl_close($ch);
+
+
+        $bytes = $size;
+
+        if ($bytes > 0)
+        {
+            $unit = intval(log($bytes, 1024));
+            $units = array('B', 'KB', 'MB', 'GB');
+
+            if (array_key_exists($unit, $units) === true)
+            {
+                return sprintf('%d %s', $bytes / pow(1024, $unit), $units[$unit]);
+            }
+        }
+
+        return $bytes;
+
+    }
+
+    /**
+     * Console output
+     *
+     * @param [string] $string
+     * @return void
+     */
+    private function info($string)
+    {
+        echo $string . PHP_EOL;
     }
 }
